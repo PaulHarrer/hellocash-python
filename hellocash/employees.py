6a5e7f9c5397d7308dc8e663c2fe0d5fb3b2251d
PaulHarrer <mail@paulharrer.at>
2024-05-31 16:49:11 +0200

    Patch: added missing employee endpoints

--- hellocash/employees.py
+++ hellocash/employees.py
@@ -2,6 +2,10 @@ class Employees:
     def __init__(self, client):
         self.client = client
 
+    def get(self, employee_id):
+        endpoint = f"v1/employees/{employee_id}"
+        return self.client._request("GET", endpoint)
+
     def list(self):
         endpoint = "v1/employees"
         return self.client._request("GET", endpoint)
